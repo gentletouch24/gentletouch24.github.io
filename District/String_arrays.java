0d9c991d4c5175671713fdd02b2aa3c295e53752
gentletouch24 <48472046+gentletouch24@users.noreply.github.com>
2020-04-15 17:54:50 +0600

    Add files via upload

---
+++ District/String_arrays.java
@@ -0,0 +1,17 @@
+package com.dae.gentletouch.mydaeapp2;
+
+/**
+ * Created by ayushi on 4/30/2017.
+ */
+
+public class String_arrays {
+    public static String[] hours_spinner = {"1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "11" , "12" , "13" , "14" , "15" , "16" , "17" , "18" , "19" , "20" , "21" , "22" , "23" , "24"};
+
+    public static String[] work_type_spinner = {"Maid" , "Cook" , "Baby Sitter" , "Nurse" , "Driver"};
+
+    public static String[] blood_spinner = {"A+" , "B+" , "AB+" , "O+" , "A-" , "B-" , "AB-" , "O-"};
+    public static String[] cadre_spinner = {"BCS(Admin)" , "BCS(Foreign Affairs)" , "BCS(Police)" , "BCS(Ansar)" , "BCS(Audit and Accounts)" , "BCS(Tax)" , "BCS(Cooperative)" , "BCS(Economic)" , "BCS(Food)" , "BCS(Info:general)" , "BCS(Family Planning)" , "BCS(Postal)" , "BCS(Fisheries)" , "BCS(Animal Science)" , "BCS(Agriculture)" , "BCS(Health)" , "BCS(Info:technical)" , "BCS(PWD:civil)" , "BCS(PWD:E/M)" , "BCS(Statistics)" , "BCS(Gen. Edu.)" , "BCS(Gen. Edu. TTC)"};
+    public static String[] lecturer = {"Lecturer(Accounting)" , "Lecturer(Agriculture)" , "Lecturer(Bangla)" , "Lecturer(Botany)" , "Lecturer(Chemistry)" , "Lecturer(Economics)" , "Lecturer(English)" , "Lecturer(Finance and Banking)" , "Lecturer(Geology)" , "Lecturer(History)" , "Lecturer(Home Economics)" , "Lecturer(Islamic History and Culture)" , "Lecturer(Management)" , "Lecturer(Marketing)" , "Lecturer(Pali)" , "Lecturer(Phylosophy)" , "Lecturer(Physics)" , "Lecturer(Political Science)" , "Lecturer(Psychology)" , "Lecturer(Social Welfare)" , "Lecturer(Sociology)" , "Lecturer(Soil Science)", "Lecturer(Statistics)", "Lecturer(Zoology)", "Lecturer(Education_TTC)", "Lecturer(English-TTC)", "Lecturer(Library Science-TTC)", "Lecturer(Political Science-TTC)", "Lecturer(Professional Ethics-TTC)", "Lecturer(Others)"};
+
+    public static String[] cities= { "Bagerhat","Barguna","Bandarban","Barishal","Bhola","Bogura","Brahmanbaria","Chandpur","Chapainawabganj","Chattogram","Chuadanga","Cox's Bazar","Cumilla","Dhaka","Dinajpur","Faridpur","Feni","Gaibandha","Gazipur","Gopalganj","Habiganj","Jamalpur","Jashore","Jhalokati","Jhenaidah","Joypurhat","Khagrachari","Khulna","Kishoreganj","Kurigram","Kushtia","Lakshmipur","Lalmonirhat","Madaripur","Magura","Manikganj","Meherpur","Moulavibazar","Munshiganj","Mymensingh","Naogaon","Narail","Narayanganj","Narshingdi","Natore","Netrokona","Nilphamari","Noakhali","Pabna","Panchagarh","Patuakhali","Pirojpur","Rajbari","Rajshahi","Rangamati","Rangpur","Satkhira","Shariatpur","Sherpur","Sirajganj","Sunamganj","Sylhet","Tangail","Thakurgaon"};
+}
