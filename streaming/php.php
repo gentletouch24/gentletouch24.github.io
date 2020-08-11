2246b56404d18ddd7e748fe8f62ce419be7b6e50
gentletouch24 <48472046+gentletouch24@users.noreply.github.com>
2020-08-11 10:05:52 +0600

    Update php.php

--- streaming/php.php
+++ streaming/php.php
@@ -1,28 +1,9 @@
-<style type="text/css">@import url("style.css");</style>
-<a href="<?php echo $_SERVER["REQUEST_URI"];?>">Home</a><hr/>
+<link href=“https://vjs.zencdn.net/6.6.3/video-js.css” rel=“stylesheet”>
+<script src=“https://vjs.zencdn.net/6.6.3/video.js”></script>
+<script src=“https://unpkg.com/videojs-flash/dist/videojs-flash.js”></script>
+<script src=“https://unpkg.com/videojs-contrib-hls/dist/video js-contrib-hls.js”></script>
 
-
-
-    <html>
-    <head>
-    <meta charset=utf-8 />
-    <title>Live TV</title>
-       <link href="video-js.css" rel="stylesheet">
-
-	   
-      <script src="video.js"></script>
-      <script src="videojs-contrib-hls.js"></script>
-       
-    </head>
-    <body>
-      <video id="playthis" class="video-js vjs-fluid vjs-default-skin" controls preload="auto" data-setup='{}'>
-        <source src="https://itpolly.iptv.digijadoo.net/live/zee_cinema/chunks.m3u8" type="application/x-mpegURL">
-      </video>
-      
-    <script>
-    var player = videojs('playthis');
-    player.play();
-    </script>
-      
-    </body>
-.............
+<video class=“video-js vjs-default-skin vjs-big-play-centered” data-setup=’{“controls”: true, “autoplay”: true }’>
+<source src=“http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8” type=“application/x-mpegURL”>
+Your browser does not support HTML5 video.
+</video>
